9f77571446244f44a977ccacd41b3745a8174448
Isaiah Moore <imoore@udel.edu>
2026-03-17 19:08:17 -0400

    erasing the pre-downloaded stuff

---
+++ calculator-app/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App(): React.JSX.Element {
+    return (
+        <div>
+            <h1>Calculator</h1>
+        </div>
+    );
+}
