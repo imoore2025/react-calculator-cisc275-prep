fc8968d714d336d2c483582b7aa0d9f8c8dcfe3e
Isaiah Moore <imoore@udel.edu>
2026-03-18 07:36:24 -0400

    Moved Tests file to its own folder. Added a test that I will use later. Created a calculatorLogic folder to write the code that will work the buttons/UI

---
+++ calculator-app/src/tests/Tests.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@jest/globals";
+
+test("basic math", () => {
+  expect(2 + 2).toBe(4);
+});
+
+
+{/**
+  import { add } from "./calculatorLogic";
+import { expect, test } from "@jest/globals";
+
+test("add returns the sum", () => {
+    expect(add(2, 3)).toBe(5);
+});
+  */}
